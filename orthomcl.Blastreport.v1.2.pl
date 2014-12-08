bd83829d745a54ce6819447205adc37c1387cc11
Surya Saha <ss2489@cornell.edu>
2014-12-08 15:04:16 -0500

    Improved docs and var names. Added handlers for empty query descriptions. Used to break for protein headers generated by prokka

--- orthomcl.Blastreport.v1.2.pl
+++ orthomcl.Blastreport.v1.2.pl
@@ -13,7 +13,7 @@ use Bio::SearchIO;
 
 =head1 NAME
 
- orthomcl.blastreport.v1.2.pl - prepare a Excel report to summarize <LAS> protein hits to labelled <Rhizobiacae> pan-proteome 
+ orthomcl.blastreport.v1.2.pl - prepare a Excel report to summarize protein hits to labelled pan-proteome 
 
 =head1 SYNOPSIS
 
@@ -23,7 +23,9 @@ use Bio::SearchIO;
 
  Reads in a blast report presuming a default e-value cutoff of 10. Print out a tab separated file with
  description of the hits for each source protein. HITS WITH EVAL < CUTOFF ARE NOT RECORDED IN EITHER OF
- THE OUTPUT FILES. Also lists the OrthoMCL groups hit by a <LAS> protein.
+ THE OUTPUT FILES. Also lists the OrthoMCL groups hit by a protein.
+ 
+ May not work if draft FAA headers not formatted like Refseq ">gi|1|art|XXX| undefined product [wACP3 draft]"
  
 =head1 VERSION HISTORY
  Version   1.0  INPUT: blast report, Mapping file 
@@ -47,13 +49,13 @@ use Bio::SearchIO;
  Command-line options can be abbreviated to single-letter options, e.g. -f instead of --file. Some options
 are mandatory (see below).
 
-   --report <.out>    Blast report in text formatText file with protein clusters from OrthoMCL run to validate (required)
-   --cutoff <float>   A float value <10>
+   --report <.out>    Blast report in text format with protein clusters from OrthoMCL run to validate (required)
+   --cutoff <float>   A float value for blast Evalue cutoff <10>
    --map    <.txt>    Tab delimited file with abbreviations followed by full genome names used in Refseq (required)
    --groups <.txt>    Text file with protein clusters from OrthoMCL run (required)
    --out    <text>    Excel report file
    --num    <INT>     Number of organisms/genomes (required)
-   --src    <text>    Abbv for source organism (required)
+   --src    <text>    Abbv for assembly (required)
       
 =head1 AUTHOR
 
@@ -62,13 +64,14 @@ are mandatory (see below).
 =cut
 
 sub getGI{
-	$_[0]=~ /^gi\|(\d+)/; return $1;
+	my (@temp); @temp=split(/\|/,$_[0]);
+	return $temp[1];
 }
 
 sub getOrg{
 	my (@temp); @temp=split(/\|/,$_[0]);
 	$temp[0]=~ s/[\s\S]+\[//;
-	$temp[0]=~ s/\]$//;
+	$temp[0]=~ s/\]$//; #remove desc prefix
 	return $temp[0];
 }
 
@@ -79,12 +82,13 @@ sub getClass{
 
 sub getDesc{
 	my (@temp); @temp=split(/\|/,$_[0]);
-	$temp[0]=~ s/\[[\s\S]+\]//;
+	$temp[0]=~ s/\[[\s\S]+\]//; #remove org name suffix
+#	if ( !defined $temp[0]){ print STDERR "*******\n".$_[0]; exit 1;}
 	return $temp[0];
 }
 
 my ($rep,$cutoff,$map,$out,$in,$result,$hit,@temp,%orgs_cnt,@orgs,@hits,$nohits,$onlyweakhits,$src,
-$weakhits,$las_annot_ctr,$las_unannot_ctr,$i,$j,$rec,%ctr,$grp,%groups,%hitgrps,%nohitgrps,$flag,$gen);
+$weakhits,$src_annot_ctr,$src_unannot_ctr,$i,$j,$rec,%ctr,$grp,%groups,%hitgrps,%nohitgrps,$flag,$gen);
 
 GetOptions (
 	'report=s' => \$rep,
@@ -107,8 +111,8 @@ $out ||="${rep}\.xls";
 defined($gen) or (system('pod2text',$0), exit 1);
 defined($src) or (system('pod2text',$0), exit 1);
 
-unless(open(OUT,">$out")){print "not able to open $out\n\n";exit 1;}
-unless(open(OUT1,">multi\.$out")){print "not able to open multi\.$out\n\n";exit 1;}
+unless(open(XLS,">$out")){print "not able to open $out\n\n";exit 1;}
+unless(open(MXLS,">multi\.$out")){print "not able to open multi\.$out\n\n";exit 1;}
 unless(open(INMAP,"<$map")){print "not able to open $map\n\n";exit 1;}
 unless(open(INGRP,"<$grp")){print "not able to open $grp\n\n";exit 1;}
 
@@ -134,28 +138,30 @@ while($rec=<INGRP>){
 }
 close (INGRP);
 
-print OUT "\t\t\tBLAST report\n\n";
-print OUT1 "\t\t\tBLAST report (multiple hits)\n\n";
-print OUT "$src protein\t\t";
+print XLS "\t\t\tBLAST report\n\n";
+print MXLS "\t\t\tBLAST report (multiple hits)\n\n";
+print XLS "$src protein\t\t";
 foreach $i (@orgs){
-	print OUT $i,"\t\t";
+	print XLS $i,"\t\t";
 }
-print OUT "\nGI\tDesc\t";
-for $i (1..$gen){print OUT "Class\tEval\t";}
-print OUT "Groups\n";
-print OUT1 "$src protein\t\tHit\nGI\tDesc\tOrg\tClass\tEval\tGroup\n";
+print XLS "\nGI\tDesc\t";
+for $i (1..$gen){print XLS "Class\tEval\t";}
+print XLS "Groups\n";
+print MXLS "$src protein\t\tHit\nGI\tDesc\tOrg\tClass\tEval\tGroup\n";
 $in = new Bio::SearchIO(-format => 'blast', -file   => $rep);
 $onlyweakhits=$weakhits=$nohits='';
-$las_annot_ctr=$las_unannot_ctr=0;
+$src_annot_ctr=$src_unannot_ctr=0;
 while($result = $in->next_result ) {
 	## $result is a Bio::Search::Result::ResultI compliant object
 	#init data structures
 	@hits=(); %ctr=%orgs_cnt;
 	#get hit data
 	if($result->num_hits>0){
-		$las_annot_ctr++; $flag=0;
+		$src_annot_ctr++; $flag=0;
 		while($hit = $result->next_hit ) {
-	    ## $hit is a Bio::Search::Hit::HitI compliant object
+	    	## $hit is a Bio::Search::Hit::HitI compliant object
+	    	##debug
+#	    	if ($hit->description eq ''){print STDERR "\n".$hit->name; exit 1;}
 	    	if ($hit->significance <= $cutoff){
 		    	$rec={
 		    		GI    => &getGI($hit->name),
@@ -173,21 +179,37 @@ while($result = $in->next_result ) {
 	    			&getOrg($hit->description)."\t".&getClass($hit->description)."\t".$hit->significance."\n";
 	    	}
 	    	elsif ($hit->significance > $cutoff){#for protein with poor hits
-	    		$weakhits=$weakhits.&getGI($result->query_name)."\t".&getDesc($result->query_description)."\t".
+	    		##debug
+#	    		if ($result->query_description eq ''){print STDERR "11111111\n".$result->query_name."\n".$hit->name;}
+	    		##to handle weird prokka protein descriptions that break parser
+	    		if ($result->query_description ne ''){
+	    			$weakhits=$weakhits.&getGI($result->query_name)."\t".&getDesc($result->query_description)."\t".
 	    			&getOrg($hit->description)."\t".&getClass($hit->description)."\t".$hit->significance."\n";
+	    		}
+	    		else{
+	    			$weakhits=$weakhits.&getGI($result->query_name)."\tNA via parser\t".
+	    			&getOrg($hit->description)."\t".&getClass($hit->description)."\t".$hit->significance."\n";
+	    		}
 	    	}
 		}
 		#print hit data
 		%hitgrps=();
-		if (@hits>0){ print OUT "\n",&getGI($result->query_name),"\t",&getDesc($result->query_description),"\t";}
+		if (@hits>0){ 
+			if ($result->query_description ne ''){
+				print XLS "\n",&getGI($result->query_name),"\t",&getDesc($result->query_description),"\t";
+			}
+			else{
+				print XLS "\n",&getGI($result->query_name),"\tNA via parser\t";
+			}
+		}
 		foreach $i (@orgs){
 			 if($ctr{$i}==0){#if no hits
-			 	print OUT "\t\t";
+			 	print XLS "\t\t";
 			 }
 			 elsif($ctr{$i}==1){#if 1 hit
 			 	foreach $j (@hits){
 			 		if($j->{ORG} eq $i){
-			 			print OUT $j->{CLASS},"\t",$j->{EVAL},"\t";
+			 			print XLS $j->{CLASS},"\t",$j->{EVAL},"\t";
 			 			#in case the hit is a protein not present in any OMCL group
 			 			if(exists $groups{$j->{GI}}){$hitgrps{$groups{$j->{GI}}}=1;} 
 			 			last;
@@ -201,50 +223,59 @@ while($result = $in->next_result ) {
 			 			$class=$j->{CLASS}; $eval=$j->{EVAL}; $gi=$j->{GI};
 			 		}
 			 	}
-			 	print OUT $class,"\t",$eval,"\t";#print best hit
+			 	print XLS $class,"\t",$eval,"\t";#print best hit
 			 	#record group ID
 			 	if(exists $groups{$gi}){ $hitgrps{$groups{$gi}}=1;}#in case the hit is protein not present in OMCL group
 			 	foreach $j (@hits){# print other hits
 			 		if (($j->{ORG} eq $i) && ($j->{EVAL} != $eval)){
-			 			print OUT1 &getGI($result->query_name),"\t",&getDesc($result->query_description),"\t";
-			 			print OUT1 $i,"\t",$j->{CLASS},"\t",$j->{EVAL};
+				    		##debug
+#				    		if ($result->query_description eq ''){print STDERR "222222\n".$result->query_name;}
+				    		##to handle weird prokka protein descriptions that break parser			 			
+				    		if ($result->query_description ne ''){
+				 			print MXLS &getGI($result->query_name),"\t",&getDesc($result->query_description),"\t";				    		
+				    		}
+				    		else{
+				 			print MXLS &getGI($result->query_name),"\tNA via parser\t";
+				    		}
+
+			 			print MXLS $i,"\t",$j->{CLASS},"\t",$j->{EVAL};
 			 			#in case the hit is a protein not present in any OMCL group
-			 			if(exists $groups{$j->{GI}}){print OUT1 "\t",$groups{$j->{GI}},"\n";}
-			 			else {print OUT1 "\n";}
+			 			if(exists $groups{$j->{GI}}){print MXLS "\t",$groups{$j->{GI}},"\n";}
+			 			else {print MXLS "\n";}
 			 		}
 			 	}
 			 }
 		}
-#		if (@hits>0) {print OUT "\n";}
+#		if (@hits>0) {print XLS "\n";}
 		#print group ids
 		@temp= keys %hitgrps;
 		foreach $i (@temp){
-			delete $nohitgrps{$i}; print OUT $i,"\t";
+			delete $nohitgrps{$i}; print XLS $i,"\t";
 		}
 	}
 	else{
-		$las_unannot_ctr++;
+		$src_unannot_ctr++;
 		$nohits=$nohits.&getGI($result->query_name)."\t".&getDesc($result->query_description)."\n";
 	}
 }
 
-print OUT1 "\n\n$src proteins with weak hits\n";
-print OUT1 "$src protein\t\tHit\nGI\tDesc\tOrg\tClass\tEval\n";
-print OUT1 $weakhits;
+print MXLS "\n\n$src proteins with weak hits\n";
+print MXLS "$src protein\t\tHit\nGI\tDesc\tOrg\tClass\tEval\n";
+print MXLS $weakhits;
 
-print OUT1 "\n\n$src proteins with only weak hits\n";
-print OUT1 "$src protein\t\tHit\nGI\tDesc\tOrg\tClass\tEval\n";
-print OUT1 $onlyweakhits;
-print OUT1 "\n\n$src proteins with no hits\n";
-print OUT1 "GI\tDesc\n";
-print OUT1 $nohits;
-print OUT1 "\n\nGroups with no hits from $src proteins\n";
+print MXLS "\n\n$src proteins with only weak hits\n";
+print MXLS "$src protein\t\tHit\nGI\tDesc\tOrg\tClass\tEval\n";
+print MXLS $onlyweakhits;
+print MXLS "\n\n$src proteins with no hits\n";
+print MXLS "GI\tDesc\n";
+print MXLS $nohits;
+print MXLS "\n\nGroups with no hits from $src proteins\n";
 @temp= keys %nohitgrps;
-foreach $i (@temp){ print OUT1 "$i\n";}
+foreach $i (@temp){ print MXLS "$i\n";}
 
-close(OUT);
-close(OUT1);
+close(XLS);
+close(MXLS);
 
-print STDERR "$las_annot_ctr $src proteins had hits (above and below threshold). $las_unannot_ctr had no hits\n";
+print STDERR "\n\n$src_annot_ctr $src proteins had hits (above and below threshold). $src_unannot_ctr had no hits\n";
 
 exit;
